98c2f3212e2711f10ccd35cc36624907803a2e11
Rudra150304 <rudra.p.s15032004@gmail.com>
2024-11-18 14:46:00 +0530

    Edited

--- vectors.cpp
+++ vectors.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-#define MAX 100
 
 int main(){
     int n;
-    vector<int> a(MAX);
+    vector<int> a;
     cout << "No. of elements: ";
     cin >> n;
+    a.resize(n);
     for(int i=0; i<n; i++){
         cin >> a[i];
     }
